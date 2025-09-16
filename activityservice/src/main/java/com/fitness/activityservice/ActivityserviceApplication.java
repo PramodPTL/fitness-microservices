f41d8ec7bde8fb31c88c9c0d67531247bf3e1594
Pramod Patel <itspramodptl@gmail.com>
2025-09-17 00:33:08 +0530

    Eureka server setup and setup other services as client

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -3,7 +3,6 @@ package com.fitness.activityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ActivityserviceApplication {
 
